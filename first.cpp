d2f9699bbb0a1ed10eaa7ab31fe5c51e355ad2e1
umar-ai <umarabbasi089@gmail.com>
2025-06-12 06:05:23 +0500

    Leader 2

--- first.cpp
+++ first.cpp
@@ -14,8 +14,9 @@ struct movieReview
     int cnic;
     string movie_name;
     string Review;
+    int rating;
 };
-
+// This is the main class which shows the total available tickets and show movielist
 class movie
 {
 protected:
@@ -27,7 +28,7 @@ public:
     {
         cout << total_tickets;
     }
-    int returnAvailableTickets()
+    static int returnAvailableTickets()
     {
         return available_tickets;
     }
@@ -38,15 +39,22 @@ public:
         cout << "3.Azadi" << endl;
         cout << "4.Lifetime" << endl;
         cout << "5.Afghani" << endl;
+        cout << "6.Sita Ramam" << endl;
+        cout << "7.Mission Impossible 7" << endl;
+        cout << "8.Kudo" << endl;
+        cout << "9.Loyality matters" << endl;
+        cout << "10.Power is power" << endl;
     }
 };
+// This class is totally responsible for file handling
 class handlingFiles : public movie
+
 {
 public:
     void bookTicket_file(int n)
     {
         ofstream file;
-        file.open("C:\\Users\\Shekhani Laptops\\Documents\\tickets.txt", ios::app);
+        file.open("tickets.txt", ios::app);
         ticket T;
         cout << "Enter your cnic" << endl;
         cin >> T.cnic;
@@ -55,21 +63,41 @@ public:
         {
             T.movie_name = "Pathan";
         }
-        if (n = 2)
+        else if (n = 2)
         {
             T.movie_name = "Shiddat";
         }
-        if (n = 3)
+        else if (n = 3)
         {
             T.movie_name = "Azadi";
         }
-        if (n = 4)
+        else if (n = 4)
         {
             T.movie_name = "LifeTime";
         }
-        if (n = 5)
+        else if (n = 5)
         {
             T.movie_name = "Afghani";
+        }
+        else if (n = 6)
+        {
+            T.movie_name = "Sita Ramam";
+        }
+        else if (n = 7)
+        {
+            T.movie_name = "Mission Impossible 7";
+        }
+        else if (n = 8)
+        {
+            T.movie_name = "Kudo";
+        }
+        else if (n = 9)
+        {
+            T.movie_name = "Loyality matters";
+        }
+        else if (n = 10)
+        {
+            T.movie_name = "Power is power";
         }
         file << T.cnic << "\t" << T.movie_name << "\t" << T.day << endl;
         file.close();
@@ -83,8 +111,8 @@ public:
         ofstream ofs;
         ticket T;
         movieReview R;
-        ifs.open("C:\\Users\\Shekhani Laptops\\Documents\\tickets.txt");
-        ofs.open("C:\\Users\\Shekhani Laptops\\Documents\\reviews.txt");
+        ifs.open("tickets.txt");
+        ofs.open("reviews.txt");
 
         ifs >> T.cnic;
         ifs >> T.movie_name;
@@ -119,9 +147,31 @@ public:
                 {
                     R.movie_name = "Afghani";
                 }
+                else if (n = 6)
+                {
+                    R.movie_name = "Sita Ramam";
+                }
+                else if (n = 7)
+                {
+                    R.movie_name = "Mission Impossible 7";
+                }
+                else if (n = 8)
+                {
+                    R.movie_name = "Kudo";
+                }
+                else if (n = 9)
+                {
+                    R.movie_name = "Loyality matters";
+                }
+                else if (n = 10)
+                {
+                    R.movie_name = "Power is power";
+                }
                 R.cnic = CNIC;
                 cout << "Enter your review in a liner" << endl;
                 cin >> R.Review;
+                cout<<"Also enter the rating for the Film 1-5"<<endl;
+                cin>>R.rating;
 
                 ofs << R.cnic << "\t" << R.movie_name << "\t" << R.Review << endl;
                 cout << "Review submitted " << endl;
@@ -135,14 +185,16 @@ public:
         }
     };
 };
+// This class is responsible for handling booking of tickets.
 class BookTickets : public handlingFiles
 {
 public:
     void bookTicket()
     {
-        if (100 != 0)
+        if (returnAvailableTickets != 0)
         {
             int n;
+
             movieList(); // from the movie class that is a parent class
             cout << "Enter your choice" << endl;
             cin >> n;
@@ -150,7 +202,8 @@ public:
         }
     };
 };
-static int availabe_tickets = 100;
+ int movie::available_tickets = 100;
+// This class handles filmreviews
 class FilmReviews : public handlingFiles
 {
 public:
@@ -161,9 +214,17 @@ public:
         cin >> Cnic;
         reviewFilm(Cnic);
     }
+    void ShowFilmReviws(){
+        cout<<"Film reviews"<<endl;
+        ifstream ifs;
+
+        ifs.open("review.txt");
+
+    }
 };
+
 int main()
 {
-    FilmReviews b;
-    b.movieReview();
+    
+    
 }
